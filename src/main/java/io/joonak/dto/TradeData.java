161ade449ec9663b952ae714d5d23cf645014a1f
Joonhak <ofnullable@gmail.com>
2019-05-27 23:36:30 +0900

    Fix dto

--- src/main/java/io/joonak/dto/TradeData.java
+++ src/main/java/io/joonak/dto/TradeData.java
@@ -4,57 +4,38 @@ import java.io.Serializable;
 
 public class TradeData implements Serializable {
 
-    private Double trade_price;
-    private Double trade_volume;
-    private Integer trade_amount;
+    private Double price;
+    private Double volume;
+    private Integer amount;
     private Double change_rate;
     private String code;
 
-    public Double getTrade_price() {
-        return trade_price;
+    public Double getPrice() {
+        return price;
     }
 
-    public void setTrade_price(Double trade_price) {
-        this.trade_price = trade_price;
+    public Double getVolume() {
+        return volume;
     }
 
-    public Double getTrade_volume() {
-        return trade_volume;
-    }
-
-    public void setTrade_volume(Double trade_volume) {
-        this.trade_volume = trade_volume;
-    }
-
-    public Integer getTrade_amount() {
-        return trade_amount;
-    }
-
-    public void setTrade_amount(Integer trade_amount) {
-        this.trade_amount = trade_amount;
+    public Integer getAmount() {
+        return amount;
     }
 
     public Double getChange_rate() {
         return change_rate;
     }
 
-    public void setChange_rate(Double change_rate) {
-        this.change_rate = change_rate;
-    }
-
     public String getCode() {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
     @Override
     public String toString() {
-        return "{trade_price=" + trade_price +
-                ", trade_volume=" + trade_volume +
-                ", trade_amount=" + trade_amount +
+        return "TradeData{" +
+                "price=" + price +
+                ", volume=" + volume +
+                ", amount=" + amount +
                 ", change_rate=" + change_rate +
                 ", code='" + code + '\'' +
                 '}';
